8cb529a6edca8f8a62d1420c45a4e041558300d4
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-11-28 10:16:08 -0500

    add headshot to home page in img tag

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -1,12 +1,10 @@
 import styles from "./Hero.module.css";
-import Image from "next/image";
-import me from "../../public/images/copePic.jpg";
 
 export default function Hero() {
     return (
         <div className={styles.hero}>
             <div >
-                <Image className={styles.image} src={me} height={300} width={225} />
+            <img src={"images/NewHeadshot.jpg"} alt="" className={styles.img} />
             </div>
             <h1>Hi, I'm Alex</h1>
             <h4>I'm a web developer interested in building beautiful and functional web applications</h4>
