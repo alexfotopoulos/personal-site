97f8ddce33e61eeb8bd7f77e33750ebbaf1c6718
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-11-28 11:12:49 -0500

    change project preview imgs and change some styling

--- components/allProjectspage/ProjectPreview.js
+++ components/allProjectspage/ProjectPreview.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./ProjectPreview.module.css";
 import Link from "next/link";
 
@@ -10,30 +9,24 @@ export default function Project(props) {
     const imagePath = `/images/projectImages/${image}`;
 
     return (
-        <div className={styles.project}>
-            <Link href={`/projects/${slug}`}>
-                <a className={styles.imageContainer}>
-                    <Image src={imagePath} height={200} width={300} layout="responsive"/>
-                </a>
-            </Link>
-            <div className={styles.projectDetails}>
-                <Link href={`/projects/${slug}`}>
-                    <a>
-                        <p>{title}</p>
-                    </a>
-                </Link>
-                <p>{date}</p>
-                <p>{excerpt}</p>
-                <ul className={styles.projectDetailsTagsList}>
-                    {tags.map(tag => (
-                        <Link key={Math.random()} href={`/projects/tags/${tag}`}>
-                            <a>
-                                <li className={styles.tag}>{tag}</li>
-                            </a>
-                        </Link>
-                    ))}
-                </ul>
+        <Link href={`/projects/${slug}`}>
+            <div className={styles.project}>
+                <img className={styles.img} src={imagePath} alt="" />
+                <div className={styles.projectDetails}>
+                    <p>{title}</p>
+                    <p>{date}</p>
+                    <p>{excerpt}</p>
+                    <ul className={styles.projectDetailsTagsList}>
+                        {tags.map(tag => (
+                            <Link key={Math.random()} href={`/projects/tags/${tag}`}>
+                                <a>
+                                    <li className={styles.tag}>{tag}</li>
+                                </a>
+                            </Link>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
+        </Link >
     );
 };
