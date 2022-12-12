b9d17103be8296ed85403baea58465a2d561afb2
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-12-12 12:26:54 -0500

    add responsiveness to home, about and contact pages

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 export default function AboutPage() {
 
     return (
-        <main className="page-container default-font-size">
+        <main className="page-container default-font-size u-margin-bottom-big">
             <div>
                 <h1 className="heading-primary u-margin-bottom-small">About me</h1>
                 <hr />
@@ -11,25 +11,31 @@ export default function AboutPage() {
                 <p>
                     I'd always been interested in understanding how computers and technology worked. When I first saw code on my computer screen, I was so intrigued by it. In 2019, I took a beginners Python course which taught me basic programming topics. I chose Python as the use of white space made it more readable for a newbie like me.
                 </p>
+                <br />
                 <p>
                     I progressed through the course, and at a certain point, I knew enough to write a small script for a game of hang man played at the command line. After a couple of days, I had it working. Though it was a small project consisting of a single script, I felt such satisfaction that I was able to create something on my own. I knew then that development was something I could pursue professionally.
                 </p>
+                <br />
                 <p>
                     In 2021, I decided to leave my career behind and forge a new professional life for myself, turning my interest in development into a career. For the last year+, I've been learning all about coding, how the internet works, and the ins and outs of web development.
                 </p>
+                <br />
                 <p>
                     As a self-taught developer, I’ve taken numerous online courses, including:
                 </p>
-                <ul>
+                <br />
+                <ul className="about-page__ul">
                     <li><a target="_blank" href="https://www.edx.org/course/introduction-computer-science-harvardx-cs50x">HarvardX CS50's Introduction to Computer Science</a></li>
                     <li><a target="_blank" href="https://www.udemy.com/course/the-web-developer-bootcamp/">The Web Developer Bootcamp 2022</a></li>
                     <li><a target="_blank" href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/">React – The Complete Guide</a></li>
                     <li><a target="_blank" href="https://www.udemy.com/course/react-nodejs-express-mongodb-the-mern-fullstack-guide/">React, NodeJS, Express & MongoDB – The MERN Fullstack Guide</a></li>
                     <li><a target="_blank" href="https://www.udemy.com/course/nextjs-react-the-complete-guide/">Next.js & React – The Complete Guide</a></li>
+                    <li><a target="_blank" href="https://www.udemy.com/course/advanced-css-and-sass/">Advanced CSS and Sass: Flexbox, Grid, Animations and More</a></li>
                     <li><a target="_blank" href="https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/">JavaScript Algorithms and Data Structures Masterclass</a></li>
                     <li><a target="_blank" href="https://www.udemy.com/course/git-and-github-bootcamp/">The Git & Github Bootcamp</a></li>
                 </ul>
             </div>
+            <br />
             <p>
                 Check out my Github profile <a href="https://www.github.com/alexfotopoulos" target="_blank">here</a>.
             </p>
@@ -39,6 +45,6 @@ export default function AboutPage() {
                     My formal education consists of a bachelor’s degree in Finance from Florida State University, and my professional experience has primarily been in the compliance space with a focus on Anti-Money Laundering. I've worked for large, multinational firms like Citibank, N.A. in Tampa, FL and fintech "neo" banks like N26 GmbH in New York, NY.
                 </p>
             </div>
-        </main >
+        </main>
     );
 };
