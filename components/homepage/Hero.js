0594441e351ccdef8eab2e88c914cb785157a421
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-12-20 11:19:27 -0500

    swap hero image with smaller file sized image and change style of contact form

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <div className={styles.hero}>
             <div >
-            <img src={"images/NewHeadshot.jpg"} alt="" className={styles.img} />
+            <img src={"images/Headshot.jpg"} alt="" className={styles.img} />
             </div>
             <h1 className={"heading-primary u-margin-bottom-big u-margin-top-medium move-in-left"}>Hi, I'm <span className={styles.name}>Alex</span></h1>
             <h2 className={"heading-tertiary move-in-right"}>I'm a web developer interested in building beautiful and functional web applications</h2>
