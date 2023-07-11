07f6d8f0b6991802133e544750ef379c0bc0c12b
Alex Fotopoulos <alex.foto.dev@gmail.com>
2023-07-11 15:40:00 -0400

    add shadow to ProjectTiles and swap Hero background image for Image component

--- src/components/sections/Hero/Hero.js
+++ src/components/sections/Hero/Hero.js
@@ -1,15 +1,25 @@
 import styles from "./Hero.module.scss";
+import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section className={styles.Hero} id="hero">
-            <div className={styles.HeroTextContainer}>
-                <h1>HI, I'M ALEX</h1>
-                <h4>
-                    I'm a web developer with a passion for building beautiful
-                    and functional web applications
-                </h4>
-            </div>
-        </section>
+        <>
+            <Image
+                src="/images/heroImage.jpg"
+                width={750}
+                height={750}
+                alt="hero image"
+                className={styles.HeroBackgroundImage}
+            />
+            <section className={styles.Hero} id="hero">
+                <div className={styles.HeroTextContainer}>
+                    <h1>HI, I'M ALEX</h1>
+                    <h4>
+                        I'm a web developer with a passion for building
+                        beautiful and functional web applications
+                    </h4>
+                </div>
+            </section>
+        </>
     );
 }
