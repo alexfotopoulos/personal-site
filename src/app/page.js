08f18facd591c248ce1b064d127ec12d053bb418
Alex Fotopoulos <alex@Alexs-MacBook-Pro.local>
2025-08-20 19:13:47 -0400

    superficial change to test Vercel deployment with node 22

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import Experience from "../components/sections/Experience/Experience";
 import Projects from "../components/sections/Projects/Projects";
 import Contact from "../components/sections/Contact/Contact";
 
+//superficial change to test Vercel deployment with node 22
+
 export default function page() {
     return (
         <main>
